aa8a8ba38b0d1ec14c6da83c2222a9ebaeeac54b
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2025-02-07 20:53:33 +0100

    Add script to create and fill initial tables

---
+++ db/populatedb.js
@@ -0,0 +1,74 @@
+#! /usr/bin/env node
+import 'dotenv/config';
+import pg from 'pg';
+import config from './config.js';
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS trainer (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR ( 255 )
+);
+
+CREATE TABLE IF NOT EXISTS type (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR ( 255 )
+);
+
+CREATE TABLE IF NOT EXISTS pokemon (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR ( 255 ),
+  type INTEGER NOT NULL,
+  trainer INTEGER NOT NULL
+);
+
+DELETE FROM trainer;
+DELETE FROM type;
+DELETE FROM pokemon;
+
+INSERT INTO trainer (name) 
+VALUES
+  ('Red'),
+  ('Blue'),
+  ('Gold'),
+  ('Silver');
+
+INSERT INTO type (name) 
+VALUES
+  ('Water'),
+  ('Grass'),
+  ('Fire'),
+  ('Poison'),
+  ('Fight'),
+  ('Flight'),
+  ('Rock'),
+  ('Ground'),
+  ('Fairy'),
+  ('Psycho'),
+  ('Ghost'),
+  ('Bug'),
+  ('Steel'),
+  ('Electro'),
+  ('Normal'),
+  ('Dark'),
+  ('Ice'),
+  ('Dragon');
+
+INSERT INTO pokemon (name, type, trainer)
+VALUES
+  ('Glurak', 3, 1),
+  ('Turtok', 1, 1),
+  ('Bisaflor', 2, 1),
+  ('Tauboss', 6, 2),
+  ('Nidoking', 4, 2)
+`;
+
+async function main() {
+    console.log('seeding ...');
+    const client = new pg.Client(config);
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+    console.log('done');
+}
+
+main();
